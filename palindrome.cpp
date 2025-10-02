6e7bcb51bf68288101fd19d58fd2ace279ad0643
elwitchcastlero-debug <elwitchcastlero@gmail.com>
2025-10-02 14:13:08 -0500

    Update palindrome.cpp

--- palindrome.cpp
+++ palindrome.cpp
@@ -1,30 +1,31 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+
 class palindrome {
     string n;
 public:
-    palindrome(string num): n(num){}
-    bool isPalindrome() {
-    for (int i = 0; i < n.length() / 2; i++) {
-        if (n[i] != n[n.length() - 1 - i]) {
-            return false;
-        }
-    }
-    return true;
-}
+    palindrome(string num) : n(num) {}
 
+    bool isPalindrome() {
+        for (int i = 0; i < n.length() / 2; i++) {
+            if (n[i] != n[n.length() - 1 - i]) {
+                return false; // Si hay una diferencia, no es palíndromo
+            }
         }
+        return true; // Si todos coinciden, sí es palíndromo
     }
 };
-int main(){
+
+int main() {
     string s;
-    cin>>s;
+    cin >> s; // Leer palabra o número
     palindrome p(s);
-    if (!p.isPalindrome()){
-        cout<<"False"<<endl;
+
+    if (!p.isPalindrome()) {
+        cout << "False" << endl;
+    } else {
+        cout << "True" << endl;
     }
-    else{
-        cout<<"True"<<endl;
-    }
-    }
-    
+
+    return 0;
+}
